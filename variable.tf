25aadc92498fdd1e3d11f373281b126e7da61b4c
Atakan Erdil <59777727+atakanerdil2559@users.noreply.github.com>
2020-04-14 16:08:21 -0500

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,24 @@
+variable "bucket_name" {
+    default = "environment-bucket"
+}
+
+variable "ami" {
+    default = "ami-0bc06212a56393ee1"
+}
+
+variable "sg_group" {
+    default = "default"
+}
+
+#variable "key_name" {
+ #   default = "mylaptop"
+  
+#}
+
+variable "region" {
+    default = "us-west-2"
+  
+}
+
+
+
