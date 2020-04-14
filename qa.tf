d5db2377fb1417d02cc4c8719c136a02cce1a2ee
Atakan Erdil <59777727+atakanerdil2559@users.noreply.github.com>
2020-04-14 16:05:53 -0500

    Create qa.tf

---
+++ qa.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "qa" {
+    ami               = "${var.ami}"
+    #region            =  "${var.region}"
+    instance_type     = "t2.medium"
+    security_groups    = ["${var.sg_group}"]
+    key_name          = "${aws_key_pair.test_key.key_name}"
+    user_data         = "${file("userdata_file")}"
+}
