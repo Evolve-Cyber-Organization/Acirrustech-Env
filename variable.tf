8f614efaf9124a3bbe46825aa7ffedf15c2d6af4
ynalbant <59777723+ynalbant@users.noreply.github.com>
2020-04-14 16:00:00 -0500

    Delete variable.tf

--- variable.tf
+++
@@ -1,9 +0,0 @@
-variable "ami" {}
-variable "instance_type" {}
-variable "key_name" {}
-variable "environment" {} 
-variable "s3_bucket" {} 
-variable "s3_folder_project" {} 
-variable "s3_folder_region" {} 
-variable "s3_folder_type" {} 
-variable "s3_tfstate_file" {} 
