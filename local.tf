9e4037abc301c8ab0c3fe874b8657ebe14a481f7
Merve Gumuscibik <59778896+mrvgms@users.noreply.github.com>
2020-04-14 13:32:42 -0400

    Delete local.tf

--- local.tf
+++
@@ -1,8 +0,0 @@
-resource "null_resource" "login" {
-    provisioner "local-exec" {
-    command = "aws eks --region ${var.region} update-kubeconfig --name ${var.cluster_name}"
-    }
-    provisioner "local-exec" {
-    command = "/tmp/kubectl get nodes"
-  }
-}
