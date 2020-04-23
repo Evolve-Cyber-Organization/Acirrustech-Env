9ad9b338dad79eee39a0d7009d9806d6eeae80ad
sezgin mutlu <sezginmutlu6@gmail.com>
2020-04-23 03:52:16 +0000

    added

--- ENVs/r53.tf
+++ ENVs/r53.tf
@@ -1,7 +1,7 @@
-resource "aws_route53_record" "bar" {
-  zone_id = "Z32OHGRMBVZ9LR"
-  name    = "dev.acirrustech.com"
-  type    = "CNAME"
-  ttl     = "60"
-  records = ["${aws_elb.bar.dns_name}"]
-}
+# resource "aws_route53_record" "bar" {
+#   zone_id = "Z32OHGRMBVZ9LR"
+#   name    = "dev.acirrustech.com"
+#   type    = "CNAME"
+#   ttl     = "60"
+#   records = ["${aws_elb.bar.dns_name}"]
+# }
