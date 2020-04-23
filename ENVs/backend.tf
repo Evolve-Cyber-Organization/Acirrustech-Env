d7b2a3df605d7d8c36ded0a57aff64c32ce9c4e4
sezgin mutlu <sezginmutlu6@gmail.com>
2020-04-23 17:50:01 +0000

    added

--- ENVs/backend.tf
+++ ENVs/backend.tf
@@ -1,7 +1,7 @@
 terraform {
 backend "s3" {
 bucket = "team1-bucket1"
-key = "team4/us-west-2/tools/dev/team4.tfstate"
+key = "team4/us-west-2/tools/prod/team4.tfstate"
 region = "us-west-2"
   }
 }
