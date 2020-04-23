68bc391665c04162b3168362d9a9845e391727a1
sezgin mutlu <sezginmutlu6@gmail.com>
2020-04-23 03:41:44 +0000

    r53 added

---
+++ ENVs/r53.tf
@@ -0,0 +1,6 @@
+resource "aws_route53_record" "bar" {
+  zone_id = "Z32OHGRMBVZ9LR"
+  name    = "dev.acirrustech.com"
+  type    = "CNAME"
+  ttl     = "60"
+  records = ["${aws_elb.bar.dns_name}"]
