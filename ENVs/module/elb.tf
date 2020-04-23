8961d2f0bee91e968bdb83aeb89526fdef0d5b37
sezgin mutlu <sezginmutlu6@gmail.com>
2020-04-23 02:22:12 +0000

    added elb

--- ENVs/module/elb.tf
+++ ENVs/module/elb.tf
@@ -1,71 +1,118 @@
-module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 3.0"
+# Create a new load balancer
+resource "aws_elb" "bar" {
+  name               = "foobar-terraform-elb"
+  availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
 
-  # name = "my-alb"
-
-  #  load_balancer_type = "application"
-   load_balancer_name = "my-alb"
-
-  vpc_id             = "${var.VPC}",
-  subnets            = [
-                               "${var.Subnet1}",      
-                               "${var.Subnet2}",
-                               "${var.Subnet3}"
-
-  ],
-  security_groups    = ["${var.security_group}"]
-
-  access_logs = {
-    bucket = "team1-logs1"
+  access_logs {
+    bucket        = "team1-bucket1"
+    bucket_prefix = "bar"
+    interval      = 60
   }
 
-  target_groups = [
-    {
-      name_prefix      = "default"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-    }
-  ]
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
 
-#   https_listeners = [
+
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "foobar-terraform-elb"
+  }
+}
+
+
+
+
+
+
+# module "alb" {
+#   source  = "terraform-aws-modules/alb/aws"
+#   version = "~> 3.0"
+
+#   # name = "my-alb"
+
+#   #  load_balancer_type = "application"
+#    load_balancer_name = "my-alb"
+
+#   vpc_id             = "${var.VPC}",
+#   subnets            = [
+#                                "${var.Subnet1}",      
+#                                "${var.Subnet2}",
+#                                "${var.Subnet3}"
+
+#   ],
+#   security_groups    = ["${var.security_group}"]
+
+#   access_logs = {
+#     bucket = "team1-logs1"
+#   }
+
+#   target_groups = [
 #     {
-#       port               = 443
-#       protocol           = "HTTPS"
-#       certificate_arn    = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+#       name_prefix      = "default"
+#       backend_protocol = "HTTP"
+#       backend_port     = 80
+#       target_type      = "instance"
+#     }
+#   ]
+
+# #   https_listeners = [
+# #     {
+# #       port               = 443
+# #       protocol           = "HTTPS"
+# #       certificate_arn    = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+# #       target_group_index = 0
+# #     }
+# #   ]
+
+#   http_tcp_listeners = [
+#     {
+#       port               = 80
+#       protocol           = "HTTP"
 #       target_group_index = 0
 #     }
 #   ]
 
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
-
-  tags = {
-    Environment = "Test"
-  }
-}
-
-# Create a new load balancer attachment
-resource "aws_autoscaling_attachment" "example-asg" {
-  autoscaling_group_name = "example-with-elb"
-  elb                    = "my-alb"
-}
+#   tags = {
+#     Environment = "Test"
+#   }
 # }
-# # # Create a new load balancer attachment
-# # resource "aws_autoscaling_attachment" "asg_attachment_bar" {
-# #   autoscaling_group_name = "${aws_autoscaling_group.asg.id}"
-# #   elb                    = "${aws_elb.bar.id}"
 
+# # Create a new load balancer attachment
+# resource "aws_autoscaling_attachment" "example-asg" {
+#   autoscaling_group_name = "example-with-elb"
+#   elb                    = "my-alb"
 # }
-# # resource "aws_autoscaling_attachment" "wordpress" {   
-# #     autoscaling_group_name = "${module.dev.this_autoscaling_group_id}"    
-# #     elb                    = "${aws_elb.wordpress.name}"
+# # }
+# # # # Create a new load balancer attachment
+# # # resource "aws_autoscaling_attachment" "asg_attachment_bar" {
+# # #   autoscaling_group_name = "${aws_autoscaling_group.asg.id}"
+# # #   elb                    = "${aws_elb.bar.id}"
 
 # # }
-# # }
+# # # resource "aws_autoscaling_attachment" "wordpress" {   
+# # #     autoscaling_group_name = "${module.dev.this_autoscaling_group_id}"    
+# # #     elb                    = "${aws_elb.wordpress.name}"
+
+# # # }
+# # # }
+
+
+
