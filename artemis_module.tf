a2eb8226974d78206af4d175cf868a829dbadbbd
mo <mkarimidevops@gmail.com>
2020-04-28 15:28:47 -0500

    psuhd

--- artemis_module.tf
+++ artemis_module.tf
@@ -2,7 +2,7 @@ module "artemis_deploy" {
   source  = "fuchicorp/chart/helm"
   deployment_name        = "artemis"
   deployment_endpoint    = "prod-chart.${var.eks_domain_name}"
-  docker_image           = ${var.docker_image}
-  docker_image_tag       = ${var.docker_image_tag}
+  docker_image           = "${var.docker_image}"
+  docker_image_tag       = "${var.docker_image_tag}"
   deployment_path        = "artmis-test"
 }
