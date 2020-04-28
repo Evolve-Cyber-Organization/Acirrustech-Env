9d6e7b435a212c4cef21b4389fccd7ab903bb4ac
mo <mkarimidevops@gmail.com>
2020-04-28 16:14:19 -0500

    tag

--- variable.tf
+++ variable.tf
@@ -18,6 +18,6 @@ variable "docker_image" {
     default = "713287746880.dkr.ecr.us-west-2.amazonaws.com/artemis-docker:0.1"
 }
 
-variable "docker_image" {
+variable "docker_image_tag " {
     default = "0.1"
 }
