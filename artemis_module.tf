996a30806267d1e40c38aa1233c8c6b9c6a1feb5
mo <mkarimidevops@gmail.com>
2020-04-28 15:52:42 -0500

    customer var added

--- artemis_module.tf
+++ artemis_module.tf
@@ -2,8 +2,11 @@ module "artemis_deploy" {
   source  = "fuchicorp/chart/helm"
   deployment_name        = "artemis"
   deployment_endpoint    = "prod-chart.${var.eks_domain_name}"
-  docker_image           = "${var.docker_image_url}"
-  docker_image_tag       = "${var.docker_image_version}"
   deployment_path        = "artmis-test"
   deployment_environment = "dev"
+
+   template_custom_vars    = {
+     docker_image           = "${var.docker_image_url}"
+     docker_image_tag       = "${var.docker_image_version}"
+  }
 }
