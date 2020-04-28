0ff370d3ae78449d3a6d79547d28f63c71ea93e2
mo <mkarimidevops@gmail.com>
2020-04-28 15:44:29 -0500

    changed

--- artemis_module.tf
+++ artemis_module.tf
@@ -2,8 +2,8 @@ module "artemis_deploy" {
   source  = "fuchicorp/chart/helm"
   deployment_name        = "artemis"
   deployment_endpoint    = "prod-chart.${var.eks_domain_name}"
-  docker_image           = "${var.docker_image}"
-  docker_image_tag       = "${var.docker_image_tag}"
+  docker_image_url           = "${var.docker_image}"
+  docker_image_version       = "${var.docker_image_tag}"
   deployment_path        = "artmis-test"
   deployment_environment = "dev"
 }
