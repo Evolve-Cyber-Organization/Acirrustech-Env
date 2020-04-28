4031306aae6cf5d68b53d2fe6793f9acf7ccc7bc
mo <mkarimidevops@gmail.com>
2020-04-28 15:49:12 -0500

    changed

--- variable.tf
+++ variable.tf
@@ -2,11 +2,11 @@ variable "deployment_endpoint" {
     default = "prod-chart.acirrustech.com"
 }
 
-variable "docker_image" {
+variable "docker_image_url" {
     default = "713287746880.dkr.ecr.us-west-2.amazonaws.com/artemis-docker:0.1"
 }
 
-variable "docker_image_tag" {
+variable "docker_image_version" {
     default = "0.1"
 }
 
