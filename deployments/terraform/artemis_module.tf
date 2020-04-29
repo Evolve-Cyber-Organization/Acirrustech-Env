fd37befb0165a92b85f38cd70303820ff3ed98ea
sezgin mutlu <sezginmutlu6@gmail.com>
2020-04-29 21:31:15 +0000

    aded dev-artemis

--- deployments/terraform/artemis_module.tf
+++ deployments/terraform/artemis_module.tf
@@ -1,9 +1,10 @@
 module "artemis_deploy" {
   source  = "fuchicorp/chart/helm"
-  deployment_name        = "artemis-sezgin2"
-  deployment_endpoint    = "sezgin-terraform.${var.eks_domain_name}"
-  deployment_path        = "sezgin"
+  deployment_name        = "dev-chart"
+  deployment_endpoint    = "dev.${var.eks_domain_name}"
+  deployment_path        = "dev-chart"
   deployment_environment = "dev"
+  
 
    template_custom_vars    = {
      docker_image           = "${var.docker_image_url}"
--- deployments/terraform/variable.tf
+++ deployments/terraform/variable.tf
@@ -1,13 +1,13 @@
 variable "deployment_endpoint" {
-    default = "sezgin-terraform.acirrustech.com"
+    default = "dev.acirrustech.com"
 }
 
 variable "docker_image_url" {
-    default = "713287746880.dkr.ecr.us-west-2.amazonaws.com/artemis-docker:0.2"
+    default = "713287746880.dkr.ecr.us-west-2.amazonaws.com/artemis-docker:0.9"
 }
 
 variable "docker_image_version" {
-    default = "0.2"
+    default = "0.3"
 }
 
 variable "eks_domain_name" {
