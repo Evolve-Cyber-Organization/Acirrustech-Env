699182350c04f5deacae3cde48c8fe2f1cc6c7b0
Laziz Mansurov <lazizmansurov4@gmail.com>
2020-04-30 13:28:00 -0500

    updated

---
+++ deployments/artemis_module.tf
@@ -0,0 +1,13 @@
+module "artemis_deploy" {
+  source  = "fuchicorp/chart/helm"
+  deployment_name        = "prod-chart"
+  deployment_endpoint    = "prod.${var.eks_domain_name}"
+  deployment_path        = "prod-chart"
+  deployment_environment = "prod"
+  
+
+   template_custom_vars    = {
+     docker_image           = "${var.docker_image_url}"
+     docker_image_tag       = "${var.docker_image_version}"
+  }
+}
