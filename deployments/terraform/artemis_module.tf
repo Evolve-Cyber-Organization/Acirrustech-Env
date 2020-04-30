7dd97c29154dc0adaf3a983abb56a2d6d7678cb0
Laziz Mansurov <lazizmansurov4@gmail.com>
2020-04-30 14:24:56 -0500

    updated

--- deployments/terraform/artemis_module.tf
+++ deployments/terraform/artemis_module.tf
@@ -2,7 +2,7 @@ module "artemis_deploy" {
   source  = "fuchicorp/chart/helm"
   deployment_name        = "prod-chart"
   deployment_endpoint    = "prod.${var.eks_domain_name}"
-  deployment_path        = "prod-chart"
+  deployment_path        = "prod-charts"
   deployment_environment = "prod"
    template_custom_vars    = {
      docker_image           = "${var.docker_image_url}"
