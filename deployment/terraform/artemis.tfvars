cde74e76c9a7bd78cb7774f2a7b76bc101231336
kliliia <kuzyshynliliia@gmail.com>
2020-05-07 15:17:45 +0000

    added

--- deployment/terraform/artemis.tfvars
+++
@@ -1,3 +0,0 @@
-eks_domain_name = "acirrustech.com"
-docker_image_url  = "713287746880.dkr.ecr.us-west-2.amazonaws.com/artemis-docker:0.2"
-docker_image_version = "0.2"
--- deployment/terraform/artemis_module.tf
+++
@@ -1,7 +0,0 @@
-module "helm_deploy" {
-  source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
-  deployment_name        = "liliia-deployment"
-  deployment_environment = "dev"
-  deployment_endpoint    = "devchart.acirrustech.com."
-  deployment_path        = "lily"
-}
